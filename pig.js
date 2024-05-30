1f69a524a62585f2756d55db7ca0a24df233618e
Cooper-Clark <cooperclarkbusiness@gmail.com>
2024-05-30 15:49:21 -0700

    Add files via upload

--- pig.js
+++ pig.js
@@ -18,7 +18,7 @@ function rollDie()
 {
     // roll boy
     return Math.floor(Math.random()*6)+1;
-    console.log("I rolled");
+    console.log("Rolling Die");
 }
 
 function bozoTurn()
@@ -28,11 +28,14 @@ function bozoTurn()
 
 function rollPig()
 {
+    console.log("Rolling in Pig");
     var result = rollDie();
     if (result != 1)
     {
     document.getElementById("dieresult").innerHTML = result;
-    document.getElementById("gameresult").innerHTML += result;
+    var temptotal = document.getElementById("usertotal").innerHTML;
+    temptotal = temptotal + result;
+    document.getElementById("usertotal").innerHTML = temptotal;
     }
     else
     {
@@ -44,4 +47,36 @@ function rollPig()
 function endPig()
 {
     // This represents the player rolling
+}
+
+function craps()
+{
+    // declaring vars
+    var d1 = Math.floor(Math.random()*6)+1;
+    var d2 = Math.floor(Math.random()*6)+1;
+    var sum = d2 + d1;
+    var crapsgameresult = "undecided";
+
+    //loss condition
+    if (sum == 7 || sum == 11)
+        {
+            crapsgameresult = "You lost!";
+        }
+    //win condition
+    else if (d1 == d2 && d1%2 == 0)
+        {
+            crapsgameresult = "You win!"
+        }
+    //idk what pushing means but this is the push condition
+    else
+        {
+            crapsgameresult = "You pushed!"
+        }
+
+    // update html vars
+    document.getElementById("d1").innerHTML = d1;
+    document.getElementById("d2").innerHTML = d2;
+    document.getElementById("sum").innerHTML = sum;
+    document.getElementById("crapsgameresult").innerHTML = crapsgameresult;
+
 }
